468793c1140545a5841f4521cfd81eb87700b5dc
babon7x <babon7x@gmail.com>
2022-07-08 03:45:29 +0700

    ekobootcamp

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,12 +5,49 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>EKO BOOTCAMP</title>
 </head>
 
-<body>
-    aselina mang?
+<body style="text-align: center">
+    <h1>SELAMAT DATANG DI EKO-BOOTCAMP 2022</h1>
+    <h2 style="color: red;">pastikan semua kebutuhan instalasi sudah ya gaes</h2>
+    <div style="text-align: left;padding-left:20px;">
+        1.Buat Folder di D , Project
+        <br>
+        2.klik Kanan Folder Project , klik "git bash here"
+        <br>
+        3.ketikan pada command line
+        <br>
+        composer create-project laravel/laravel:^8.0 ekobootcamp
+        <br>
 
+        <img src="img/img1.png" alt="">
+        <br>
+        tunggu hinggal selesai
+        <br>
+        4.ketikan pada command line
+        <br>
+        cd ekobootcamp
+        <br>
+        <img src="img/img2.png" alt="">
+        <br>
+        <br>
+        setelah command line berada dalam folder project kita
+        <br>
+        ketikan <br>
+        php artisan serve
+        <br>
+        ini adalah perintah menjalankan project laravel kita
+        <br>
+        buka browser lalu akses localhost:8000
+        <br>
+        <img src="img/img3.png" alt="" width="1200" height="800">
+        <br>
+        <br>
+        <br>
+        <br>
+        kalo tidak muncul seperti itu , banyak2 berdoa saja okey !!
+    </div>
 </body>
 
 </html>

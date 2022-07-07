aa9675bc6fae94ad8c5f9a9195f8b2219c2c77bd
babon7x <babon7x@gmail.com>
2022-07-08 04:00:54 +0700

    bersihkan tampilan

---
+++ resources/views/tutor2.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>EKO BOOTCAMP</title>
+</head>
+
+<body style="text-align: center">
+    <h1>SELAMAT DATANG DI EKO-BOOTCAMP 2022</h1>
+    <h2>Membersihkan Tampilan</h2>
+    <div style="text-align: left;padding-left:20px;">
+      1. buka text editor favorite kamu
+      <br>
+      <br>
+      2. buka project kita dengan text editormu
+      <br>
+      <img src="img/img5.png" alt="">
+      <br>
+      <br>
+      3. klik file nya , lihat coding nya
+      <br>
+      <br>
+      4. ubah jadi seperti ini
+      <br>
+      <img src="img/img4.png" alt="">
+      <br>
+      5. jalankan project nya
+      <br>
+    </div>
+
+</body>
+
+</html>
